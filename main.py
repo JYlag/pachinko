d6b1349263c5dcebdda937725efa431663c37487
JYlag <joeylagan@gmail.com>
2017-09-30 15:14:40 -0700

    Add python and flask/ templates+static folders

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from flask import redirect, render_template, session, request, flash
 from app import app, db
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def runGame():
+    return render_template('game.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
