133fab85eb2641f1f4b3eac199c50d7944b28126
Sigala Hernandez <sigalahr11@gmail.com>
2017-10-16 08:59:01 -0700

    Added Features to Heroku

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'mysql+pymysql://adventure-game:adventure-game@localhost:8889/adventure-game')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://adventure-game:adventure-game@localhost:8889/adventure-game')
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
